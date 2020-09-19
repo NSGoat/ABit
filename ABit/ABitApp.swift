63a5bd65735bcf1a9cecd790b3fa6deeeae4d7dd
Ed Rutter <ed@rutterstudios.co.uk>
2020-09-19 12:30:06 +0100

    Initial Commit

---
+++ ABit/ABitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABitApp.swift
+//  ABit
+//
+//  Created by Ed Rutter on 19/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct ABitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
